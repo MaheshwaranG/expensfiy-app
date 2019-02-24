df1acd2c42330b6a23183491871e3c9541e7b112
MaheshwaranG <gmaheshwaranit@gmail.com>
2019-02-24 12:08:31 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import 'react-dates/lib/css/_datepicker.css';
 import { firebase }from './firebase/firebase';
 import LoadingPage from './components/LoadingPage';
 
-// example Test sample
+// example Test sample Rewrite
 
 const store = configureStore();
 const jsx = (
