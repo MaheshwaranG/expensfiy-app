eeefc58e1b79f644379ab51ba9415a8f7711fbed
maheshwaranG <gmaheshwaranit@gmail.com>
2019-02-24 12:38:24 +0530

    Changes need to only in Master Branch

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import 'react-dates/lib/css/_datepicker.css';
 import { firebase }from './firebase/firebase';
 import LoadingPage from './components/LoadingPage';
 //  commit 1
-// commit 2
+// commit 23456
 
 
 const store = configureStore();
